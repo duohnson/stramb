5ade67fdca7d9ecca8ca95a7d4de4d6b99a73154
Daniel Uohnson <peadser98@gmail.com>
2025-12-20 10:51:29 -0600

    updates

--- tienda/models.py
+++ tienda/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=0)
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
 
